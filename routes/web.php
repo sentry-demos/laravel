8e05c3abcb1d04769dc8ae6d187e1d386fd9a75f
William Capozzoli <thinkocapo@gmail.com>
2019-06-12 13:56:58 -0700

    json decoded

--- routes/web.php
+++ routes/web.php
@@ -41,19 +41,39 @@ Route::get('/unhandled', function () {
 
 });
 
+// error_log($request->json());
+// error_log($request.post());
+// both work
+// error_log("1 "."2");
+// error_log("1 ".$order);
 Route::post('/checkout', ['middleware' => 'cors',function (Request $request) {
-    error_log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    // error_log($request->json());
-    error_log($request->getContent());
-    // error_log($request.post());
+    error_log("11111");
+
+    $order = $request->getContent();
+    error_log($order);
+    // error_log($order->email); // "Trying to get a property of a non-object"
+    // error_log($order['email']); // "Illegal string offset 'email'"
+
+    // $someJSON = json_encode($order);
+    // error_log($someJSON->email); // "Trying to get a property of a non-object"
+    // error_log($someJSON['email']); // "Illegal string offset 'email'"
+
+    $myobj = json_decode($order);
+    error_log($myobj->email);
 
 
-    // order = json.loads(request.data)
-    // print "Processing order for: " + order["email"]
+
+    // error_log("Processing order for: ".$order->email);    
     // cart = order["cart"]
     // process_order(cart)
 
-    return $request;
+
+//     $someObject = ...; // Replace ... with your PHP Object
+//   foreach($someObject as $key => $value) {
+//     echo $value->name . ", " . $value->gender . "<br>";
+//   }
+
+    return 'successful';
     // return response('Hello World', 200)
     //               ->header('Access-Control-Allow-Origin', '*')
     //               ->header('Access-Control-Allow-Headers', 'Content-Type')

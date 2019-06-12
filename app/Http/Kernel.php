52530fe31daee6ceeb365040823f4bc451a0910a
William Capozzoli <thinkocapo@gmail.com>
2019-06-11 19:24:31 -0700

    comment

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -70,6 +70,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => \App\Http\Middleware\Cors::class // barryvdh ?
+        'cors' => \App\Http\Middleware\Cors::class // try barryvdh ?
     ];
 }

3c61fe2c62d1a0ede9f685aba9ec23f08e11c281
William Capozzoli <thinkocapo@gmail.com>
2019-06-18 16:33:00 -0700

    modularized helper functions and checkout works

--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,7 @@ include 'inventory.php';
 use App\Exceptions\Handler;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-
 use Illuminate\Support\Facades\Cache;
-
-/**
- * DEV NOTES
- * no global scope
- * can't persist state between http requests
- * I put Inventory in separate file (a separate process) and web.php can access it, but not form within the api route
- * no objects with {} marks, though laravel's () is an option
- * associative array is alternative to an object, but will be harder to iterate through and use in process_order
- * can't get $global keyword to work according to https://www.php.net/manual/en/language.types.object.php
- * idea - create a Factory that doesn't touch a real DB but returns dummy data? synchronously
- * idea - try using 'config' modules in Laravel, but are these immutable?
- * idea - is there a way to reference inventory.php from within the /checkout route?
- * idea - laravel's Cache fake database...?
- */
-
-// DEV NOTE    
-// works, the variable comes from inventory.php
-// error_log(json_encode($inventory));
-
 
 /*
 |--------------------------------------------------------------------------
@@ -32,11 +12,7 @@ use Illuminate\Support\Facades\Cache;
 */
 
 
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/logout', 'Auth\LoginController@logout')->name('logout' );
 
 Route::get('/handled', function (Request $request) {
     try {
@@ -52,7 +28,39 @@ Route::get('/unhandled', function () {
     1/0;
 });
 
-function check_cache() {
+// TODO - tag scope, email, inventory, cart, other? see Flask demo
+Route::post('/checkout', ['middleware' => 'cors',function (Request $request) {
+    load_cache();
+    $payload = $request->getContent();
+    
+    $hammers = Cache::get('hammer');
+    error_log('HAMMERS IN STOCK ~~~~~~ ' . $hammers);
+
+    $order = json_decode($payload);
+    $cart = $order->cart;
+
+    process_order($order->cart);
+
+    return 'successful';
+
+}]);
+
+function decrementStock($item) {
+    Cache::decrement($item->id, 1);
+    error_log("SUCCESS");
+}
+function getInventory() {
+    $inventory = new StdClass();
+    $inventory->wrench = Cache::get('wrench');
+    $inventory->nails = Cache::get('nails');
+    $inventory->hammer = Cache::get('hammer');
+    return $inventory;
+}
+function isOutOfStock($item) {
+    $inventory = getInventory();
+    return $inventory->{$item->id} <= 0;
+}
+function load_cache() {
     if (!Cache::has('wrench')) {
         Cache::increment('wrench', 1);        
     }
@@ -63,68 +71,14 @@ function check_cache() {
         Cache::increment('hammer', 1);
     }
 }
-
-class Inventory {
-    var $wrench;
-    var $nails;
-    var $hammer;
-    public function __construct() 
-    {
-        $this->wrench = Cache::get('wrench');
-        $this->nails = Cache::get('nails');
-        $this->hammer = Cache::get('hammer');
-    }
-
-    public function getItem($item) {
-        error_log("getting item..." . $item);
-        return $this->$item;
-    }
-
-    public function decrement($item) {
-        $this->$item = $this->$item - 1; // -= 1;
-    }
-
-}
-
 function process_order(array $cart) {
-    $Inventory = new Inventory();
-    error_log("Inventory" . json_encode($Inventory));
-
-    $tempInventory = $Inventory;
-    error_log("tempInventory" . json_encode($tempInventory));
-
     foreach ($cart as $item) {
         error_log("cart item " . json_encode($item));
 
-        error_log("GGGGG " . $Inventory->getItem("nails"));
-
-        if ($Inventory->getItem($item->id) <= 0) {
-            error_log("FAILURE no Inventory");
+        if (isOutOfStock($item)) {
+             error_log("FAILURE no Inventory");
         } else {
-            $tempInventory->decrement($item); // -=1;
-            error_log("SUCCESS"); // error_log("Success: " . $item["id"] . " was purchased, remaining stock is " . str($tempInventory[$item["id"]])
+            decrementStock($item);
         }
     }
-    $Inventory = $tempInventory;
 }
-
-// TODO - tag scope, email, inventory, cart, other? see Flask demo
-Route::post('/checkout', ['middleware' => 'cors',function (Request $request) {
-    check_cache();
-
-    // TODO - remove
-    // $value = Cache::get('wrench');
-    // error_log('1111111 ' . $value);
-    // Cache::increment('wrench', 1);
-
-
-    $payload = $request->getContent();
-    $order = json_decode($payload);
-    error_log($order->email);
-
-    process_order($order->cart);
-    // cart = order["cart"]
-
-    return 'successful';
-
-}]);

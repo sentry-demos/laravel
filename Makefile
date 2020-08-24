81d1fac8adc70e4b166d96e5e0d2eee7b3f780f6
sentryadam0000345 <adam.toth-fejel@sentry.io>
2020-08-24 09:25:34 -0700

    Cleaned up the code.

--- Makefile
+++ Makefile
@@ -5,6 +5,11 @@
 SENTRY_ORG=testorg-az
 SENTRY_PROJECT=sentry-demos-laravel
 VERSION=`sentry-cli releases propose-version`
+COMMIT_SHA=$(shell git rev-parse HEAD)
+WHOAMI=$(shell whoami)
+GCP_SERVICE_NAME=laravel-errors
+GCP_WORKSPACE_NAME=workspace_laravels_errors
+REPOSITORY=us.gcr.io/sales-engineering-sf
 
 setup_release: create_release associate_commits serve
 
@@ -12,7 +17,18 @@ create_release:
 	sentry-cli releases -o $(SENTRY_ORG) new -p $(SENTRY_PROJECT) $(VERSION)
 
 associate_commits:
-	sentry-cli releases -o $(SENTRY_ORG) -p $(SENTRY_PROJECT) set-commits --auto $(VERSION)
+	-sentry-cli releases -o $(SENTRY_ORG) -p $(SENTRY_PROJECT) set-commits --auto $(VERSION)
 
 serve:
 	php artisan cache:clear && php artisan serve
+
+# GCP
+deploy_gcp: build_image deploy_service
+
+build_image:
+	gcloud builds submit --substitutions=COMMIT_SHA=$(COMMIT_SHA) --config=cloudbuild.yaml
+
+deploy_service:
+	gcloud run deploy $(WHOAMI)-$(GCP_SERVICE_NAME) --image $(REPOSITORY)/$(GCP_WORKSPACE_NAME):$(COMMIT_SHA) --platform managed
+
+.PHONY: setup_release create_release associate_commits serve deploy_gcp
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,46 +19,23 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        
 
             header("Access-Control-Allow-Origin: *");
-    
+
             // ALLOW OPTIONS METHOD
             $headers = [
                 'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
                 'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin, x-transaction-id'
             ];
-            error_log("HI");
-            error_log($request);
-            error_log($request->getMethod());
+
             if($request->getMethod() == "OPTIONS") {
-                error_log("ZAPP");
-                // The client-side application can set only headers allowed in Access-Control-Allow-Headers
                 return Response::make(500, $headers);
             }
-    
+
             $response = $next($request);
             foreach($headers as $key => $value)
                 $response->header($key, $value);
             return $response;
-        
- 
-        // error_log("GOT TO HANDLE");
-        // error_log($request);
-        // if ($request->Access-Control-Request-Method:'POST'){
-        //     $response = Response::make();
-        //     error_log("ZAPPER");
-        // } else {
-        //     $response = $next($request);
-        // }
-        // // return $next($request)
-        // return $response
-        //     ->header('Access-Control-Allow-Origin', '*')
-        //     ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        //     ->header('Access-Control-Allow-Headers', '*');
-
-        //create a react project, create a backend project GCP
-        //label accordingly
     }
 }
 
